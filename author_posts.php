a4dabe37d8321fdaf859e39e3561fced0327cdeb
vasko <vjelovac90@gmail.com>
2018-09-11 00:23:43 +0200

    adding some changes

--- author_posts.php
+++ author_posts.php
@@ -68,12 +68,6 @@
     <!-- footer including -->
     <?php include "includes/footer.php" ?>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
 </body>
 
 </html>
